712520052c8df14b3c55d3a32c48cbf63eed728b
Харьковец Агния <agnia.kharkovets@yandex.ru>
2025-09-29 00:48:03 +0300

    Виселица

--- senla/src/hangman/hangman.java
+++ senla/src/hangman/hangman.java
@@ -1,5 +1,201 @@
 package hangman;
 
-public class hangman {
+import java.util.Scanner;
+import java.util.Random;
+
+class HangmanGame {
+    private String secretWord;
+    private char[] guessedLetters;
+    private int remainingLives;
+    private String usedLetters;
     
+    public HangmanGame() {
+        String[] wordList = {"ВАМПИР", "ТРЕЗУБЕЦ", "ЭКСПЕРИМЕНТ", "ОГНЕТУШИТЕЛЬ"};
+        
+        Random random = new Random();
+        this.secretWord = wordList[random.nextInt(wordList.length)];
+        this.guessedLetters = new char[secretWord.length()];
+        
+        // Заполняем массив подчеркиваниями
+        for (int i = 0; i < guessedLetters.length; i++) {
+            guessedLetters[i] = '_';
+        }
+        
+        this.remainingLives = 6;
+        this.usedLetters = "";
+    }
+    
+    public boolean guessLetter(char letter) {
+
+        letter = Character.toUpperCase(letter);
+        
+        if (usedLetters.indexOf(letter) != -1) {
+            System.out.println("Вы уже использовали букву '" + letter + "'");
+            return false;
+        }
+        
+        usedLetters += letter;
+        boolean found = false;
+        
+        for (int i = 0; i < secretWord.length(); i++) {
+            if (secretWord.charAt(i) == letter) {
+                guessedLetters[i] = letter;
+                found = true;
+            }
+        }
+        
+        if (!found) {
+            remainingLives--;
+        }
+        
+        return found;
+    }
+    
+    public boolean isWordGuessed() {
+        return new String(guessedLetters).equals(secretWord);
+    }
+    
+    public boolean isGameOver() {
+        return remainingLives <= 0;
+    }
+    
+    public String getGuessedWord() {
+        String result = "";
+        for (char c : guessedLetters) {
+            result += c + " ";
+        }
+        return result;
+    }
+    
+    public int getRemainingLives() {
+        return remainingLives;
+    }
+    
+    public String getUsedLetters() {
+        return usedLetters;
+    }
+    
+    public String getSecretWord() {
+        return secretWord;
+    }
+}
+
+class HangmanDrawer {
+    public static void drawHangman(int lives) {
+        String[] stages = {
+            """
+              ____
+              |  |
+                 |
+                 |
+                 |
+                 |
+            _____|___
+            """,
+            """
+              ____
+              |  |
+              O  |
+                 |
+                 |
+                 |
+            _____|___
+            """,
+            """
+              ____
+              |  |
+              O  |
+              |  |
+                 |
+                 |
+            _____|___
+            """,
+            """
+              ____
+              |  |
+              O  |
+             /|  |
+                 |
+                 |
+            _____|___
+            """,
+            """
+              ____
+              |  |
+              O  |
+             /|\\ |
+                 |
+                 |
+            _____|___
+            """,
+            """
+              ____
+              |  |
+              O  |
+             /|\\ |
+             /   |
+                 |
+            _____|___
+            """,
+            """
+              ____
+              |  |
+              O  |
+             /|\\ |
+             / \\ |
+                 |
+            _____|___
+            """
+        };
+        
+        System.out.println(stages[6 - lives]);
+    }
+}
+
+public class hangman {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        HangmanGame game = new HangmanGame();
+        
+        System.out.println("=== ИГРА 'ВИСЕЛИЦА' ===");
+        System.out.println("Угадайте слово по буквам!");
+        
+        while (!game.isGameOver() && !game.isWordGuessed()) {
+            System.out.println("\n" + "=".repeat(30));
+            
+            HangmanDrawer.drawHangman(game.getRemainingLives());
+            System.out.println("\nСлово: " + game.getGuessedWord());
+            System.out.println("Осталось попыток: " + game.getRemainingLives());
+            System.out.println("Использованные буквы: " + game.getUsedLetters());
+            
+            System.out.print("Введите букву: ");
+            String input = scanner.nextLine();
+            
+            if (input.length() != 1) {
+                System.out.println("Пожалуйста, введите одну букву!");
+                continue;
+            }
+            
+            char letter = input.charAt(0);
+
+            if (!Character.isLetter(letter)) {
+                System.out.println("Пожалуйста, введите букву!");
+                continue;
+            }
+            
+            game.guessLetter(letter);
+        }
+        
+        System.out.println("\n" + "=".repeat(30));
+        HangmanDrawer.drawHangman(game.getRemainingLives());
+        
+        if (game.isWordGuessed()) {
+            System.out.println("\n Да! Вы угадали слово: " + game.getSecretWord());
+        } else {
+            System.out.println("\n💀 GAME OVER! Загаданное слово было: " + game.getSecretWord());
+        }
+        
+        scanner.close();
+    }
 }
--- senla/src/password/password.java
+++ senla/src/password/password.java
@@ -1,62 +1,44 @@
 package password;
 
 import java.util.Scanner;
-import java.security.SecureRandom;
+import java.util.Random;
 
 public class password {
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
         
-    System.out.println("Введите желаемую длину пароля(от 8 до 12): ");
-    int length = scanner.nextInt();
-    if (length < 8) {
-        System.out.println("Ошибка! Длина меньше допустимой. Установлена длина 8.");
-        length = 8;
-        }
-    else if (length > 12) {
-        System.out.println("Ошибка! Длина больше допустимой. Установлена длина 12.");
-        length = 12;
-        }
-
-    String result = generatePassword(length);
-    System.out.println("Ваш пароль: " + result);
-    }
-
-    public static String generatePassword(int length) {
-    String lowercase = "abcdefghijklmnopqrstuvwxyz";
-    String uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    String digits = "0123456789";
-    String special = "!@#$%^&*()-_=+[]{}|;:,.<>?";
-
-    String allcharacters = lowercase + uppercase + digits + special;
-
-    SecureRandom random = new SecureRandom();
-    StringBuilder password = new StringBuilder();
-
-    // Гарантируем наличие хотя бы одного символа из каждой категории
-        password.append(lowercase.charAt(random.nextInt(lowercase.length())));
-        password.append(uppercase.charAt(random.nextInt(uppercase.length())));
-        password.append(digits.charAt(random.nextInt(digits.length())));
-        password.append(special.charAt(random.nextInt(special.length())));
+        System.out.print("Введите длину пароля (8-12): ");
+        int length = scanner.nextInt();
         
-        // Заполняем оставшуюся часть пароля
+        if (length < 8) {
+            System.out.println("Длина меньше 8. Установлено значение 8.");
+            length = 8;
+        } else if (length > 12) {
+            System.out.println("Длина больше 12. Установлено значение 12.");
+            length = 12;
+        }
+        
+        String chars = "abcdefghijklmnopqrstuvwxyz";
+        String CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String numbers = "0123456789";
+        String symbols = "!?,.@#$%^&*";
+        
+        String all = chars + CHARS + numbers + symbols;
+        
+        String password = "";
+
+        password += chars.charAt(random.nextInt(chars.length()));
+        password += CHARS.charAt(random.nextInt(CHARS.length()));
+        password += numbers.charAt(random.nextInt(numbers.length()));
+        password += symbols.charAt(random.nextInt(symbols.length()));
+
         for (int i = 4; i < length; i++) {
-            password.append(allcharacters.charAt(random.nextInt(allcharacters.length())));
+            password += all.charAt(random.nextInt(all.length()));
         }
         
-        // Перемешиваем символы для устранения предсказуемости
-        return shuffleString(password.toString(), random);
-    }
-    
-    // Метод для перемешивания символов в строке
-    private static String shuffleString(String input, SecureRandom random) {
-        char[] characters = input.toCharArray();
-        for (int i = 0; i < characters.length; i++) {
-            int randomIndex = random.nextInt(characters.length);
-            char temp = characters[i];
-            characters[i] = characters[randomIndex];
-            characters[randomIndex] = temp;
-        }
-        return new String(characters);
+        System.out.println("Пароль: " + password);
+
+        scanner.close();
     }
 }
